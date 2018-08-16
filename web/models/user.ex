c45c3fc221e92b61f5b3436d779676d6eceb00ed
Emika Ijuin <emikaijuin@gmail.com>
2018-08-16 16:33:29 +0800

    Create User model with schema and changeset

---
+++ web/models/user.ex
@@ -0,0 +1,20 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  schema "users" do
+    field :first_name , :string
+    field :last_name , :string
+    field :provider , :string
+    field :token , :string
+    field :email, :string
+
+    timestamps
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token]) # Include first and last names later
+    |> validate_required([:email, :provider, :token])
+  end
+
+end
